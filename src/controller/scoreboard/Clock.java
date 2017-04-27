0225d87929a9a3fbf96b622890030aa26bd17237
Bradley Turek <TurekBot@gmail.com>
2017-04-27 15:19:05 -0600

    Getting ScoreBoard ready. Clock is now a custom component, too.

--- src/controller/scoreboard/Clock.java
+++ src/controller/scoreboard/Clock.java
@@ -1,7 +1,57 @@
 package controller.scoreboard;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
 /**
  * In normal mode, acts as a stop watch, counting up to see how long it takes the player to win.
  */
-public class Clock {
+public class Clock extends Label {
+
+    private IntegerProperty timeProperty = new SimpleIntegerProperty(0);
+
+    public Clock() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "clock.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        fxmlLoader.setClassLoader(getClass().getClassLoader());
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        // FIXME: 4/27/2017 Maybe this needs to go in the initialize method?
+        //Bind label to time
+        this.textProperty().bind(timeProperty.asString());
+
+    }
+
+    // FIXME: 4/27/2017 Maybe, methods are not the way to go here, but maybe they are.
+    /**
+     * Increments the clock internally, which will change what the label
+     * displays externally.
+     */
+    public void increment() {
+        timeProperty.add(1);
+    }
+
+    /**
+     * Decrements the clock internally, which will change what the label
+     * displays externally.
+     */
+    public void decrement() {
+        timeProperty.subtract(1);
+    }
+
+//    @FXML
+//    protected void doSomething() {
+//        System.out.println("The button was clicked!");
+//    }
 }
